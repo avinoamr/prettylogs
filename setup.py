850b2c524ccfcfe42cd76d79ef5c86b3ec973b67
Roi Avinoam <avinoamr@gmail.com>
2013-04-23 23:23:54 +0300

    Setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = "prettylogs",
+    version = "0.1",
+    description = "A Python logging utility for easily creating simple, beautiful and minimalistic log lines in your code.",
+    author = "Roi Avinoam",
+    author_email = "avinoamr@gmail.com",
+    url = "https://github.com/avinoamr/prettylogs",
+    license = "MIT",
+    py_modules = [ "prettylogs" ]
+)
